9fcbd423dbf42d9f44a08c62b1dd77e513d2855e
plll0123 <jsh951227@gmail.com>
2024-11-22 15:33:40 +0900

    chore: 타임리프 의존성 추가

--- core-api/build.gradle.kts
+++ core-api/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation(project(":core-domain"))
     implementation(project(":client"))
 }
